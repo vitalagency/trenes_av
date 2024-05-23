cf82bcc095f876c8059c984422dd6d8162ad2147
Sophiaalbarran <166433566+Sophiaalbarran@users.noreply.github.com>
2024-05-23 15:30:47 -0600

    cambio sin importancia

--- VagonPasajeros.h
+++ VagonPasajeros.h
@@ -1,10 +1,8 @@
 
 #ifndef VAGONPASAJEROS_H
 #define VAGONPASAJEROS_H
-#include <string>
-#include "Vagon.h"
-using namespace std;
 
+#include "Vagon.h"
 
 // Clase Vagon Pasajeros
 
@@ -28,36 +26,19 @@ class Reserva: public Vagon{
     auto nombreReserva;
 
     public:
-    int precio;
-
-    virtual:
+    
     int numAsientos;
+    Reserva(auto nombreReserva, int numAsientos);
+
+
+    virtual int numAsientos;
+    virtual int ClasePajero;
+
     
 
 };
 
 
-// Clase Precios
 
-// Precio Turista  $3.00 
-class PrecioTurista: public Vagon{
-    private:
-    string nombre; 
-
-    public:
-    int precioTurista;
-
-}
-
-// Precio Lujo  $9.00 
-
-class PrecioLujo: public Vagon{
-    private: 
-    string nombre;
-
-    public:
-    int precioLujo;
-
-}
 
 #endif
